7600a64bc51199b76e94a707728d564415f6609a
Carlos Almanza <itscarlosalmanza@gmail.com>
2020-10-29 21:36:45 -0600

    LINKED SCRIPT2 FILE TO HTML & SET VAR's  IN SCRIPT2

--- assets/script2.js
+++ assets/script2.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     //JQUERY METHODS GO WILL GO HERE
 
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+    $.get("apiUrl", function (data) {
+        console.log(data);
+    })
+
+
+
 
 
 
