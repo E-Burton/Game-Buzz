c9a55a919f7c5b8fc21cbc2af179afe290e36ead
Carlos Almanza <itscarlosalmanza@gmail.com>
2020-11-03 21:29:48 -0700

    Continued to work in the code for script2, calling to the cocktail Api

--- assets/script2.js
+++ assets/script2.js
@@ -2,29 +2,31 @@
 
 $(document).ready(function () {
     //JS METHODS GO WILL GO HERE
-    var apiKey = "1"
-    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+    var ingredient = "Rum"
+    //Need an event listener for the choice of liquor
+    //Need an if/else statement for user input
+    //Need to concatnate the user input to url
+    //Need to Finish!!!!
 
-    function get() {
-        $.ajax({
-            url: "apiUrl",
-            type: 'POST',
-            function(data) {
-                console.log(data);
-            },
-
-        });
-    }
-
+    $.ajax({
+        url: apiUrl + ingredient,
+        success: function (data) {
+            console.log(data);
 
 
 
 
+            //var div = document.createElement("div");
+            //document.getElementById("myDiv").appendChild(div);
 
 
 
+        },
 
+
+    })
 
 
 
