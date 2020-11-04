b437ab09758be3f7351b2d815c046386c611e848
Carlos Almanza <itscarlosalmanza@gmail.com>
2020-11-03 18:56:09 -0700

    Continued code in script2 file

--- assets/script2.js
+++ assets/script2.js
@@ -1,11 +1,23 @@
-$(document).ready(function () {
-    //JQUERY METHODS GO WILL GO HERE
+//Javascript for the cocktail API call
 
+$(document).ready(function () {
+    //JS METHODS GO WILL GO HERE
+    var apiKey = "1"
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-    $.get("apiUrl", function (data) {
-        console.log(data);
-    })
+
+    function get() {
+        $.ajax({
+            url: "apiUrl",
+            type: 'POST',
+            function(data) {
+                console.log(data);
+            },
+
+        });
+    }
+
+
 
 
 
